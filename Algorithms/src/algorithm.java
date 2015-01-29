e8324dc9c5c77a54759cd7851159915b43da2ef0
xuannie <alexiscyx@gmail.com>
2015-01-28 22:16:57 -0600

    Updated Algorithm homework - complete

--- Algorithms/src/algorithm.java
+++ Algorithms/src/algorithm.java
@@ -11,9 +11,41 @@ import java.util.Scanner;
 public class algorithm {
 
 	public static Scanner kb = new Scanner(System.in);
-	protected static int n, x;
 	
 	public static void main(String[] args) {
+<<<<<<< HEAD
+		int n, x, output;
+		
+		System.out.print("Enter product: ");
+		x = kb.nextInt();
+		
+		do{
+			System.out.print("Enter exponent: ");
+			n = kb.nextInt();
+			if (n<0)
+				System.out.println("Invalid number.");
+		}while(n<0);
+		
+		output = power(x,n);
+		
+		System.out.println("Output of " + x + "^" + n + " is " + output + ".");
+	}
+
+	private static int power(int x, int n) {
+		// TODO Auto-generated method stub
+		int y;
+		if (n==0){
+			return 1;
+		}
+		if (n%2==1){
+			y = power(x, (n-1)/2);
+			return x*y*y;
+		}
+		else{
+			y = power(x,n/2);
+			return y*y;
+		}
+=======
 		String cons, exp;
 		do{
 			System.out.print("Enter consonant: ");
@@ -37,6 +69,7 @@ public class algorithm {
 				System.out.println("Not a valid number. Try again.");
 			}		
 		return number;
+>>>>>>> ebc3bd862264a7b6aca057ed3fb65a442144c269
 	}
 
 }
